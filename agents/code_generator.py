843e1362e742fc308681b98fa67a363e79710384
Fletcher Wilson <wilsonf@southwestern.edu>
2026-06-01 08:53:54 -0500

    generator start

--- agents/code_generator.py
+++ agents/code_generator.py
@@ -13,3 +13,31 @@
 # send script to optimization agent
 
 
+import ollama
+import PySpice
+import os
+
+BENCHMARK_FILE_PATH = "../tasks"
+SEM_FILE_PATH = "../SEM"
+
+def fetch_SEM(task_type: str) -> list[dict]:
+    '''fetch general rules and task-specific heuristics if a task type file is found'''
+    res = []
+    
+    # always get general notes
+    with open(os.path.join(SEM_FILE_PATH, "general.md")) as f:
+        general = {"role": "notes", "content": f.read()}
+        res.append(general)
+    
+    # if there are existing notes specific to this task type, get those too
+    task_specific = os.path.join(SEM_FILE_PATH, f"/{task_type}.md")
+    if os.path.exists(task_specific):
+        with open(task_specific) as f:
+            specific = {"role": "notes", "content": f.read()}
+            res.append(specific)
+    
+    return res
+
+def generate_script(task: dict, SEM_notes: list[str], observations: list[dict]) -> str:
+
+    SEM_notes = fetch_SEM(task["circuit_type"])
