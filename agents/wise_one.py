21c4d0cd2774952dff0dff68fdd42dcf85287744
Fletcher Wilson <wilsonf@southwestern.edu>
2026-06-02 13:30:32 -0500

    quick wise one commit

--- agents/wise_one.py
+++ agents/wise_one.py
@@ -1,4 +1,7 @@
-from schema.py import CheckResult, IterationRecord
+from schema import CheckResult, IterationRecord
+import ollama
+import os
+
 
 def curate(iteration: IterationRecord, task: dict) -> None:
     ''' 
@@ -9,10 +12,23 @@ def curate(iteration: IterationRecord, task: dict) -> None:
     optimization_results = [f"Stage: {check.stage} | Passed: {check.passed} | Summary: {check.message} | Details: {check.details} \n" for check in iteration.checks]
 
     context = [
-        {"role": "system", "content": "PROMPT PLACEHOLDER"},
+        {"role": "system", "content": 
+                            '''PROMPT PLACEHOLDER    Include specific details about what to include in response so '''
+        },
         {"role": "user", "content": f"This is a circuit that {"passed" if iteration.accepted else "failed"} on iteration {iteration.attempt} for a {task["name"]} ({task["description"]}), which is of type {task["circuit_type"]}"},
         {"role": "user", "content": f"PySpice Script: {iteration.script}"},
         {"role": "user", "content":f"Record from optimization stage: {str(optimization_results)}"}, 
         {"role": "user", "content": f"Repair plan from Optimization agent {iteration.repair_plan}"}
-
     ]
+
+    response = ollama.chat(model="qwen3.5:9b", messages=context)
+    text = response["messge"]["context"]
+    file_path = "?"
+
+
+
+
+def write_to_SEM(file_path: str, knowledge: str) -> None:
+    
+    if not os.path.exists(f"SEM/{file_path}"):
+        os.mkdir(f"SEM/{file_path}")
