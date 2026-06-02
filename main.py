94b4be9ee09357e767aa7b4cc9d74deba844f578
Fletcher Wilson <145033473+fwilson12@users.noreply.github.com>
2026-06-02 14:56:56 -0500

    main script wireframe

--- main.py
+++ main.py
@@ -28,3 +28,47 @@
 #
 #     budget exhausted
 #     return None
+
+
+
+from schema import IterationRecord, CheckResult
+from optimizer import validate_and_optimize
+from wise_one import curate
+from code_generator import generate_script
+
+
+def circuit_time(task: dict, max_attempts: int) -> list[IterationRecord]:
+
+
+    SEM_additions = []
+    record_book = []
+
+    for i in range(max_attempts):
+        
+        script = generate_script(task)
+
+        IterRecord = validate_and_optimize(i, task, script)
+        record_book.append(IterRecord)
+    
+        SEM_addition = curate(IterRecord, task)
+        SEM_additions.append(f'SEM Addition for iteration {i}: {SEM_addition} \n\n')
+
+    print(SEM_additions)
+    return record_book
+
+
+def view_record_book(records: list[IterationRecord]) -> None:
+    pass
+
+
+def main(task: dict, max_attempts: int) -> None:
+    
+    record_book = circuit_time()
+
+    # view_record_book(record_book)
+
+
+if __name__ == '__main__':
+    main()
+
+    
